fe6eff4b7031976970b0bce7f9935089ebca5697
danielpolk <danielpolk@mac.com>
2019-02-28 22:26:47 -0600

    adding handlebar files

--- public/app.js
+++ public/app.js
@@ -5,13 +5,22 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-        $("#articles").append("<div class='select' data-id='" + data[i]._id + "'>" + "<div class='title'>" + data[i].title + "</div>" + "<br />" + "<div class='link'>" + data[i].link + "</div>" + "</div>");
+        $("#articles").prepend("<div data-id='" + data[i]._id + "'>" + "<h2>" + data[i].title + "</h2><p>" + data[i].summary + "</p><a target='_blank' href='" + data[i].link + "'>Read Article</a><br><button id='makeNoteHTML' class='btn btn-outline-secondary'  data-id='" + data[i]._id + "'>Make a Note</button><button class='btn btn-outline-secondary'  data-id='" + data[i]._id + "' id='saveArticleHTML' data-title='" + data[i].title + "' data-summary='" + data[i].summary + "' data-link='" + data[i].link + "'>Save Article</button></div><hr>");
     }
 });
 
+// ============== ON CLICK TO GOES TO SCRAPE ROUTE
+$(document).on("click", "#scrape", function () {
+    $("#articles").empty();
+    $.get("/scrape");
+    console.log("Spurs News Scrape Complete");
+});
 
-// Whenever someone clicks a .select tag
-$(document).on("click", ".select", function() {
+
+
+// ============== ON CLICK TO MAKE NOTE (GOES TO NOTE ROUTE)
+$(document).on("click", "#makeNoteHTML", function () {
+    console.log("help");
     // Empty the notes from the note section
     $("#notes").empty();
     // Save the id from the p tag
@@ -23,28 +32,45 @@ $(document).on("click", ".select", function() {
         url: "/articles/" + thisId
     })
     // With that done, add the note information to the page
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
+            // $("#notes").addClass("notes_padding");
             // The title of the article
-            $("#notes").append("<h5>" + data.title + "</h5>");
+            $("#notes").append("<h2>" + data.title + "</h2>");
             // An input to enter a new title
-            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<div class='form-row'><div class='form-group col-md-12'><h4>Title:</h4><input id='titleinput' name='title' ></div></div>");
             // A textarea to add a new note body
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<div class='form-row'><div class='form-group col-md-12'><h4>Body:</h4><textarea id='bodyinput' name='body'></textarea></div></div>");
             // A button to submit a new note, with the id of the article saved to it
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-            // A button to delete note, with the id of the article saved to it
-            $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
+            $("#notes").append("<button class='btn btn-outline-secondary' data-id='" + data._id + "' id='savenote' data-title='" + data.title + "' data-summary='" + data.summary + "'>Save Note</button>");
+
 
             // If there's a note in the article
             if (data.note) {
+                console.log("DATA NOTE APP.JS LINE 46");
                 // Place the title of the note in the title input
                 $("#titleinput").val(data.note.title);
                 // Place the body of the note in the body textarea
                 $("#bodyinput").val(data.note.body);
+                $("#notes").append("<button><a href='/notes/" + data._id + "'>See Note</a></button>");
             }
         });
+
 });
+
+// ============== ON CLICK TO SEE ALL NOTES
+$(document).on("click", "#allNotes", function () {
+    $.ajax({
+        method: "GET",
+        url: "/notes/"
+    })
+    // With that done, add the note information to the page
+        .then(function (data) {
+            console.log(data);
+            res.JSON(data);
+        });
+});
+
 
 // When you click the savenote button
 $(document).on("click", "#savenote", function() {
@@ -76,30 +102,78 @@ $(document).on("click", "#savenote", function() {
 });
 
 // When you click the deletenote button
-$(document).on("click", "#deletenote", function() {
+$(document).on("click", "#deleteNoteHTML", function() {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-
-    // Run a POST request to delete the contents of the note.
+    var data_title = $(this).attr("data-title");
+    var data_summary = $(this).attr("data-summary");
+    var data_link = $(this).attr("data-link");
+  
     $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
+        method: "DELETE",
+        url: "/notes/" + thisId,
         data: {
-            // Value taken from title input
-            title: $("#titleinput").val(""),
-            // Value taken from note textarea
-            body: $("#bodyinput").val("")
+            title: data_title,
+            body: data_summary,
+            link: data_link
         }
     })
     // With that done
-        .then(function(data) {
+        .then(function (data) {
             // Log the response
             console.log(data);
-            // Empty the notes section
-            $("#notes").empty();
+            location.reload();
+
+        });
+});
+
+// ============== ON CLICK TO DELETED SAVED
+$(document).on("click", "#deleteSavedHTML", function () {
+    var thisId = $(this).attr("data-id");
+    var data_title = $(this).attr("data-title");
+    var data_summary = $(this).attr("data-summary");
+    var data_link = $(this).attr("data-link");
+
+    $.ajax({
+        method: "DELETE",
+        url: "/saved/" + thisId,
+        data: {
+            title: data_title,
+            body: data_summary,
+            link: data_link
+        }
+    })
+    // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            location.reload();
+
+        });
+});
+
+// ============== ON CLICK TO SAVE ARTICLE (GOES TO SAVE ROUTE)
+$(document).on("click", "#saveArticleHTML", function () {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+    var data_title = $(this).attr("data-title");
+    var data_summary = $(this).attr("data-summary");
+    var data_link = $(this).attr("data-link");
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "POST",
+        url: "/saved/" + thisId,
+        data: {
+            title: data_title,
+            body: data_summary,
+            link: data_link
+        }
+    })
+    // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
         });
 
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Initialize Express
+var app = express();
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -11,10 +21,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
-
-// Initialize Express
-var app = express();
+// var PORT = 3000;
 
 // Configure middleware
 
@@ -27,11 +34,20 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/spursdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/spursdb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/spursdb";
+
+mongoose.connect(MONGODB_URI);
+
+
+
 
 // Routes
-
-// A GET route for scraping the echoJS website
+// HOME
+app.get("/", function (req, res) {
+    res.render("index");
+});
+// A GET route for scraping the spurs website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.poundingtherock.com/").then(function(response) {
@@ -39,17 +55,22 @@ app.get("/scrape", function(req, res) {
         var $ = cheerio.load(response.data);
 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("div h2").each(function(i, element) {
+        $(".c-entry-box--compact__body").each(function(i, element) {
             // Save an empty result object
             var result = {};
 
             // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
+                .children("h2")
                 .children("a")
                 .text();
             result.link = $(this)
+                .children("h2")    
                 .children("a")
                 .attr("href");
+            result.summary = $(this)
+                .children("p")
+                .text();
 
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
@@ -68,57 +89,208 @@ app.get("/scrape", function(req, res) {
     });
 });
 
+
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
+app.get("/articles", function (req, res) {
     db.Article.find({})
-        .then(function(dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
-            res.json(dbArticle);
+        .then(function (dbScrape) {
+            // If all Notes are successfully found, send them back to the client
+            res.json(dbScrape);
         })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
+        .catch(function (err) {
+            // If an error occurs, send the error back to the client
             res.json(err);
         });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
+app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({
+        _id: req.params.id
+    })
         .populate("note")
-        .then(function(dbArticle) {
-            // If we were able to successfully find an Article with the given id, send it back to the client
-            res.json(dbArticle);
+        .then(function (dbScrapeInfo) {
+            res.json(dbScrapeInfo);
         })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
-            res.json(err);
+        .catch(function (error) {
+            res.json(error);
         });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
+app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
-        .then(function(dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                note: dbNote._id
+            }, {
+                new: true
+            });
         })
-        .then(function(dbArticle) {
-            // If we were able to successfully update an Article, send it back to the client
+        .then(function (dbArticle) {
+            // If the User was updated successfully, send it back to the client
             res.json(dbArticle);
         })
-        .catch(function(err) {
-            // If an error occurred, send it to the client
+        .catch(function (err) {
+            // If an error occurs, send it back to the client
             res.json(err);
+        });
+
+});
+
+// SEE ARTICLE NOTES
+app.get("/notes/:id", function (req, res) {
+    db.Article.findOne({
+        _id: req.params.id
+    })
+        .populate("note")
+        .then(function (dbScrapeInfo) {
+            res.render("notes", {
+                articleNotes: dbScrapeInfo
+            });
+        }).catch(function (error) {
+            res.json(error);
+        });
+});
+
+// SEE ALL NOTES
+app.get("/notes/", function (req, res) {
+    db.Note.find({})
+        .populate("note")
+        .then(function (dbScrapeInfo) {
+            //res.json(dbScrapeInfo);
+            res.render("allnotes", {
+                articleNotes: dbScrapeInfo
+            });
+        }).catch(function (error) {
+            res.json(error);
+        });
+});
+
+// NOTES DELETE ROUTE
+app.delete("/notes/:id", function (req, res) {
+    console.log("DELTEROUTE");
+    db.Note.findOne({
+        _id: req.params.id
+    }).deleteOne()
+    //db.Saved.deleteOne({_id: req.params.id})
+        .then(function (dbNotes) {
+            // View the added result in the console
+            console.log(dbNotes);
+            return db.Saved.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                note: dbNotes._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbNotes) {
+            // If the User was updated successfully, send it back to the client
+            //res.json(dbNotes);
+            res.render("saved", {
+                savedArticles: dbNotes
+            });
+        })
+        .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
+        });
+});
+
+// SAVED POST ROUTE
+app.post("/saved/:id", function (req, res) {
+    db.Saved.create(req.body)
+        .then(function (dbSaved) {
+            // View the added result in the console
+            console.log(dbSaved);
+            return db.Article.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                saved: dbSaved._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbSaved) {
+            // If the User was updated successfully, send it back to the client
+            //res.json(dbSaved);
+            res.render("saved", {
+                savedArticles: dbSaved
+            });
+        })
+        .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
+        });
+});
+
+// INDIVIDUAL SAVED ROUTE GET FOR DELETE BUTTONS
+app.get("/saved/:id", function (req, res) {
+    db.Saved.findOne({
+        _id: req.params.id
+    })
+        .populate("note")
+        .then(function (dbScrapeInfo) {
+            res.render("saved", {
+                savedArticles: dbScrapeInfo
+            });
+        }).catch(function (error) {
+            res.json(error);
+        });
+});
+
+// SAVED GET ROUTE
+app.get("/saved", function (req, res) {
+    db.Saved.find({})
+        .populate("saved")
+        .then(function (dbScrapeInfo) {
+            //res.json(dbScrapeInfo);
+            console.log(dbScrapeInfo);
+            res.render("saved", {
+                savedArticles: dbScrapeInfo
+            });
+
+        })
+        .catch(function (error) {
+            res.json(error);
+        });
+});
+
+// SAVED DELETE ROUTE
+app.delete("/saved/:id", function (req, res) {
+    console.log("DELTEROUTE");
+    db.Saved.findOne({
+        _id: req.params.id
+    }).deleteOne()
+    //db.Saved.deleteOne({_id: req.params.id})
+        .then(function (dbSaved) {
+            // View the added result in the console
+            console.log(dbSaved);
+            return db.Saved.findOneAndUpdate({
+                _id: req.params.id
+            }, {
+                saved: dbSaved._id
+            }, {
+                new: true
+            });
+        })
+        .then(function (dbSaved) {
+            // If the User was updated successfully, send it back to the client
+            //res.json(dbSaved);
+            res.render("saved", {
+                savedArticles: dbSaved
+            });
+        })
+        .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
         });
 });
 
 // Start the server
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("App running on port "  + "!");
 });
